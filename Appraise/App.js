4517b97205d537fa48903fe2fcfbd3ec370e97af
mvaleriani <michaelvaleriani@gmail.com>
2018-08-15 21:55:54 -0700

    style rework

--- Appraise/App.js
+++ Appraise/App.js
@@ -16,7 +16,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
   },
