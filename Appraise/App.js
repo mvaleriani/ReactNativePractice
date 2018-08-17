9c5481fb7ea772e8f3e14fffdf7ba9321c58014a
mvaleriani <michaelvaleriani@gmail.com>
2018-08-16 22:14:00 -0700

    retire files and move on to Flutter framework

--- Appraise/App.js
+++ Appraise/App.js
@@ -13,10 +13,13 @@ export default class App extends React.Component {
   }
 }
 
+
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 7,
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center'
   },
